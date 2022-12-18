9cd9d24f63bb790660b0a962575d1a02a2ff9488
PlitRoyce <danielmglbraga@gmail.com>
2022-12-18 18:19:08 -0300

    Init projeto clone-netflix

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "a5e9dc51f513f04d4a0273da8a308446"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
